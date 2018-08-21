1cfcb067ad863f015e203292fc88749b52a10fea
ayodeleoduduwa <ayodeleoduduwa52@gmail.com>
2018-08-20 22:43:46 -0300

    Segunda aula java

---
+++ AulaJAVA12/src/matematica/Ayodele.java
@@ -0,0 +1,32 @@
+package matematica;
+
+public class Ayodele {
+	public static void main(String args[])  {
+		int x = 16, y = 22,total = 0;
+		
+	
+	System.out.println(total);
+	total = x + y;
+	
+		
+		if(total > 38) {
+			System.out.println(total);
+			
+			}else if (total == 38) {
+				System.out.println("O valor é igual a 38");
+			}else {
+				System.out.println("o valor é inferior a 38");
+				
+			}
+		int z = 1;
+		while(z <= 100) {
+			System.out.println(z);
+		z = z + 2;}
+		
+		for(int p = 100; p > 0; p = p - 1) {
+			System.out.println(p);
+		}
+		
+	}
+
+}
